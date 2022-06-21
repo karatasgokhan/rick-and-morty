a939ebcdb3f23d561052a9806af1ddb24a312e12
gokhanKaratass <78848175+gokhanKaratass@users.noreply.github.com>
2022-06-21 18:30:51 +0300

    Create not found page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Container, Row, Col } from "react-grid-system";
+import NotFound from "../assets/rickAndMorty404.png";
+import * as ROUTES from "../constans/routePath";
+import s from "./PageNotFound.module.css";
 
 function PageNotFound() {
   return (
     <>
-      <div>PageNotFound</div>
+      <div className="mainContent">
+        <Container>
+          <Row>
+            <Col sm={12}>
+              <div className={s.notFoundWrapper}>
+                <p>404</p>
+                <p>Page Not Found</p>
+                <img src={NotFound} alt="Rick And Morty page not found" />
+                <a href={ROUTES.HOME}>Click to Return to Homepage</a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 }
